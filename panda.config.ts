dd4c1d277b3062deb562ee0ffd4a7220f88c7420
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2024-05-23 21:00:44 +0800

    ignore .vscode and narrowed panda search area

--- panda.config.ts
+++ panda.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from "@pandacss/dev";
+import { defineConfig } from "@pandacss/dev"
 
 export default defineConfig({
   preflight: true,
-  presets: ['@pandacss/preset-base', '@park-ui/panda-preset'],
-  include: ['./src/**/*.{js,jsx,ts,tsx}'],
+  presets: ["@pandacss/preset-base", "@park-ui/panda-preset"],
+  include: [
+    "./src/routes/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+  ],
   exclude: [],
-  jsxFramework: 'solid',
-  outdir: 'styled-system',
-  outExtension: "js"
-});
+  jsxFramework: "solid",
+  outdir: "styled-system",
+  outExtension: "js",
+})
